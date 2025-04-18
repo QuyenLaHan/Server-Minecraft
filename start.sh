3ce57d0ab982738aced71da6720e411410e34a37
QuyenLaHan <90555121+QuyenLaHan@users.noreply.github.com>
2025-04-19 00:44:35 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-java -Xms1G -Xmx1G -jar server.jar nogui
+
+# Nếu chưa có jar, tự tải về
+if [ ! -f purpur-1.20.4.jar ]; then
+  curl -o purpur-1.20.4.jar https://api.purpurmc.org/v2/purpur/1.20.4/latest/download
+fi
+
+java -Xms1G -Xmx1G -jar purpur-1.20.4.jar nogui
