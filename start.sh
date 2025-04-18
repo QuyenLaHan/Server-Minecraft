9fe31b541fc4f59ffea3861182565d0fc76b6bfe
QuyenLaHan <90555121+QuyenLaHan@users.noreply.github.com>
2025-04-19 00:55:27 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Nếu chưa có jar, tự tải về
+# Tải Purpur nếu chưa có
 if [ ! -f purpur-1.20.4.jar ]; then
-  curl -o purpur-1.20.4.jar https://api.purpurmc.org/v2/purpur/1.20.4/latest/download
+  echo "Đang tải Purpur 1.20.4 bằng wget..."
+  wget -O purpur-1.20.4.jar https://api.purpurmc.org/v2/purpur/1.20.4/latest/download
 fi
 
+# Chạy server
 java -Xms1G -Xmx1G -jar purpur-1.20.4.jar nogui
